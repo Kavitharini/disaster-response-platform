b6b67e5651f0d34de9ee974eb5ee9c9f3f32d640
Kavitharini <kalpanakavi512@gmail.com>
2026-03-13 16:02:47 +0530

    Resolved all merge conflicts

--- frontend/script.js
+++ frontend/script.js
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
 async function submitRequest(){
 
-const name=document.getElementById("name").value
-const location=document.getElementById("location").value
-const message=document.getElementById("message").value
+const name = document.getElementById("name").value
+const location = document.getElementById("location").value
+const message = document.getElementById("message").value
 
 await fetch("http://127.0.0.1:5000/submit",{
 method:"POST",
@@ -24,38 +23,18 @@ loadRequests()
 
 async function loadRequests(){
 
-const res=await fetch("http://127.0.0.1:5000/requests")
-const data=await res.json()
+const res = await fetch("http://127.0.0.1:5000/requests")
+const data = await res.json()
 
-const list=document.getElementById("requests")
+const list = document.getElementById("requests")
 list.innerHTML=""
 
 data.forEach(req=>{
-const li=document.createElement("li")
-li.textContent=req.name+" - "+req.location+" - "+req.message
+const li = document.createElement("li")
+li.textContent = req.name + " - " + req.location + " - " + req.message
 list.appendChild(li)
 })
 
 }
 
 loadRequests()
-=======
-body{
-font-family: Arial;
-text-align:center;
-background:#f5f5f5;
-}
-
-input, textarea{
-width:300px;
-padding:10px;
-margin:10px;
-}
-
-button{
-background:red;
-color:white;
-padding:10px 20px;
-border:none;
-}
->>>>>>> 22bcf6c517bfdf745ad6dea879afcad1a9214dab
